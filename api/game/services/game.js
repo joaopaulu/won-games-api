90b29b11a2fb920fc5755e96cd034f87e38d4212
jp <jptick@gmail.com>
2020-11-15 11:10:57 -0300

    make scrapper many to many

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -1,11 +1,7 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
- * to customize this service
- */
-
 const axios = require("axios")
+const slugify = require("slugify");
 
 async function getGameInfo(slug){
   const jsdom = require('jsdom');
@@ -31,14 +27,59 @@ async function getGameInfo(slug){
   }
 }
 
+async function getByName(name, entityName){
+  const item = await strapi.services[entityName].find({ name })
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName){
+  const item = await getByName(name, entityName)
+
+  if(!item){
+    return await strapi.services[entityName].create({
+      name,
+      slug:slugify(name, { lower: true }),
+    });
+  }
+}
+
+async function createManyToManyData(products){
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const {developer, publisher, genres, suportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    suportedOperatingSystems &&
+      suportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ])
+}
+
 module.exports = {
   populate: async (params) =>{
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
 
     const { data: { products }} = await axios.get(gogApiUrl)
 
-    //console.log(products[0].slug)
+    await createManyToManyData([products[2], products[3]])
 
-    console.log(await getGameInfo(products[0].slug))
+
   }
 };
